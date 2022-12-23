798da22edd913f88045ead199ca99c66f030ca7d
Berutotion <eagle163013@gmail.com>
2022-12-23 21:46:22 +0800

    compelet attendance module

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <script setup>
 import {reactive, ref} from 'vue'
 import axios from 'axios'
+import dayjs from "dayjs"
 const name =ref("")
 const password = ref("")
 const token = ref("")
+const user = ref({})
 const isLogin = ref(false)
+
+const url = 'http://localhost:8080/api'
+
+const AtWork = ref(false)
 // const userData = reactive({})
 
 function login(){
-  axios.post("http://localhost:8080/login",{
+  axios.post("http://localhost:8080/api/login",{
     name : name.value,
     password : password.value
   }).then((res) =>{
-    token.value = res.data.data
+    user.value = res.data.user
+    token.value = res.data.token
+    AtWork.value = res.data.AtWork
     isLogin.value = true
   }).catch((error) =>{
     console.log(error)
@@ -30,23 +38,50 @@ function data(){
 }
 
 function test(){
-  console.log("token" , token.value)
-  axios.get("http://localhost:8080/test",
-  {headers: { Authorization: `Bearer ${token.value}` },
+  console.log("user" , user.value.jobId)
+  // axios.get("http://localhost:8080/test",
+  // {headers: { Authorization: `Bearer ${token.value}` },
+  //   })
+  // .then((res) =>{
+  //   console.log(res.data)
+  // })
+  // .catch((err) =>{
+  //   console.log(err.response.data)
+  // })
+}
+
+async function  check(){
+  try {
+    if(AtWork.value){
+    //下班
+     const res = await axios.post(`${url}/user/check`,{
+      jobId:user.value.id,
+      time:dayjs().format("YYYY/MM/DD HH:mm")
     })
-  .then((res) =>{
     console.log(res.data)
-  })
-  .catch((err) =>{
-    console.log(err.response.data)
-  })
+    //AtWork.value = false
+  }else{
+    //上班
+    const res = await axios.post(`${url}/user/check`,{
+      jobId:user.value.id,
+      time:dayjs().format("YYYY/MM/DD HH:mm")
+    })
+    console.log(res.data)
+    AtWork.value = true
+  }
+  } catch (error) {
+    console.log(error)
+  }
+  
 }
 </script>
 
 <template>
 <div v-if="isLogin">
   <p>User data</p>
-  <button @click='data'>Test</button>
+  <button @click='test'>Test</button>
+  <button v-if="AtWork" @click="check">下班</button>
+  <button v-else @click="check">上班</button>
 </div>
   <div v-else>
     <label for="name">使用者</label>
